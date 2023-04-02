5d7a42e561674ba183ec944292ff03225d21a915
StarFoxS20 <127164017+StarFoxS20@users.noreply.github.com>
2023-04-02 20:53:06 -0300

    Tarefa_notas_mod8

---
+++ Notas.java
@@ -0,0 +1,29 @@
+public class Notas {
+
+    public static void main(String args[]){
+
+        //Como faço para o usuário digitar as notas
+        //que nem nas aulas de introdução como o comando (read)?
+
+        double nota1 = 5.6;
+        double nota2 = 7.3;
+        double nota3 = 8.2;
+        double nota4 = 4.9;
+
+        double media = (nota1 + nota2 + nota3 + nota4)/4;
+
+
+
+        System.out.println("A média é:" + media) ;
+
+    }
+
+
+
+
+
+
+
+
+
+}
